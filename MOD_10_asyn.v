d6ccd13e829899b4847a0db392702a817462bf69
Md. Mahim Babu <127214556+Mahim-111@users.noreply.github.com>
2024-10-08 16:29:37 +0600

    Update MOD_10_asyn.v

--- MOD_10_asyn.v
+++ MOD_10_asyn.v
@@ -2,11 +2,15 @@ module counter(q, q_bar, clk, clr);
 	output [3:0] q;
 	output [3:0] q_bar;
 	input clk,clr;
+	wire clr1;
 
-	JK JK1(1'b1, 1'b1, clr, clk,  q[0], q_bar[0]);
-	JK JK2(1'b1, 1'b1, clr, q[0], q[1], q_bar[1]);	
-	JK JK3(1'b1, 1'b1, clr, q[1], q[2], q_bar[2]);	
-	JK JK4(1'b1, 1'b1, clr, q[2], q[3], q_bar[3]);
+	nand(p, q[1], q[3]);
+	and(clr1, p, clr);
+
+	JK JK1(1'b1, 1'b1, clr1, clk,  q[0], q_bar[0]);
+	JK JK2(1'b1, 1'b1, clr1, q[0], q[1], q_bar[1]);	
+	JK JK3(1'b1, 1'b1, clr1, q[1], q[2], q_bar[2]);	
+	JK JK4(1'b1, 1'b1, clr1, q[2], q[3], q_bar[3]);
 	
 endmodule
 
